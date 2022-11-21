6fecaf320173c3ddce4ee7c25fb5558ab6d6bfea
Rayfy07 <thibautbourdon77@gmail.com>
2022-11-21 14:47:58 +0100

    added project front

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -1,3 +1,21 @@
 <?php
 
+namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+final class HomeController extends AbstractController 
+{
+
+    #[Route(path: "/project", name: "Project")]
+    public function project(Request $request): Response
+    {
+        return $this->render('project/project.html.twig');
+    }
+}
