ca1f016c193df8e2675088c3156bfdb1fa3f7e1d
Fan2Shrek <pierre27.ambroise@gmail.com>
2022-11-21 14:01:54 +0100

    added entities

---
+++ src/Entity/Host.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Classes;
+
+use App\Traits\CodeNotesTrait;
+use App\Traits\IdTrait;
+use App\Traits\NameTrait;
+use App\Interfaces\CodeNoteInterface;
+use App\Interfaces\NameInterface;
+use App\Interfaces\IdInterface;
+
+class Host implements IdInterface, nameInterface, codeNoteInterface{
+
+    use IdTrait, NameTrait, CodeNotesTrait;
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $code,
+        private string $notes,
+    )
+    {
+    }
+    public function __toString()
+    {
+        return $this->id;
+    }
+}
